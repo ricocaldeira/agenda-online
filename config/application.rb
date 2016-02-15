1b9ce6993bcf3e07605e77daf740c882ad9844cf
Ricardo Caldeira <ricardo.nezz@gmail.com>
2016-02-14 23:11:19 -0200

    Removes mailer sender and unnecessary fix for static assets on Heroku

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,5 @@ module AgendaOnline
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
   end
 end
